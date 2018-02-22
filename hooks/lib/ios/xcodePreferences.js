a71bbc862bd2bd7c236a1af2ab70976c27a421f8
Oren Gershoni <oren@memeni.com>
2018-02-22 09:30:08 +0000

    test

--- hooks/lib/ios/xcodePreferences.js
+++ hooks/lib/ios/xcodePreferences.js
@@ -145,18 +145,13 @@ function createPbxFileReference(xcodeProject, entitlementsRelativeFilePath) {
 function loadProjectFile() {
   var platform_ios;
   var projectFile;
-
-  try {
-    // try pre-5.0 cordova structure
-    platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms')['ios'];
-    projectFile = platform_ios.parseProjectFile(iosPlatformPath());
-  } catch (e) {
-    // let's try cordova 5.0 structure
-    platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms/ios');
-    projectFile = platform_ios.parseProjectFile(iosPlatformPath());
-  }
-
-  return projectFile;
+    
+    var iosPlatformApi = require(path.join(iosPlatformPath(), '/cordova/Api'));
+    var projectFileApi = require(path.join(iosPlatformPath(), '/cordova/lib/projectFile.js'));
+    var locations = (new iosPlatformApi()).locations;
+    projectFile = projectFileApi.parse(locations);
+    
+    return projectFile;
 }
 
 /**
